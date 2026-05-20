0e5b224b8caa8a64bba7e92f870b022aaed6c51e
Tuan Le <peter.le@lixibox.com>
2026-05-20 13:53:34 +0700

    Initial Commit

---
+++ F1Widget/F1WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1WidgetApp.swift
+//  F1Widget
+//
+//  Created by Tuan Le on 20/5/26.
+//
+
+import SwiftUI
+
+@main
+struct F1WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
